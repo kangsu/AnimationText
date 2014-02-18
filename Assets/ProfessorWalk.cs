f69a7ed24121d620156eac8eca56bda0e23e9af3
unknown <kagatlin@MININT-3OJN2OF.redmond.corp.microsoft.com>
2014-02-17 21:39:12 -0800

    Initial commit for animation test.

---
+++ Assets/ProfessorWalk.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProfessorWalk : MonoBehaviour {
+
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		animator.SetInteger ("Direction", 1);
+		if (animator) {
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				animator.SetInteger ("Direction", 0);
+			} else if (Input.GetKey (KeyCode.DownArrow)) {
+				animator.SetInteger ("Direction", 1);
+			} else if (Input.GetKey (KeyCode.RightArrow)) {
+				animator.SetInteger ("Direction",3);
+			} else if (Input.GetKey (KeyCode.LeftArrow)) {
+				animator.SetInteger ("Direction", 2);
+			}
+		}
+	}
+
+
+}
