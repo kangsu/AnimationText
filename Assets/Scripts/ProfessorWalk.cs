701fa29ec5eec97d8974658332535fda9fc084c4
Kang Su <joandkangsu@gmail.com>
2014-05-18 16:09:47 -0700

    Delete Uneeded
    Delete some non-needed files.

--- Assets/Scripts/ProfessorWalk.cs
+++ Assets/Scripts/ProfessorWalk.cs
@@ -31,9 +31,15 @@ public class ProfessorWalk : MonoBehaviour {
 
     void FixedUpdate()
     {
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rigidbody2D.velocity = new Vector2(0, -10);
+
+            if (transform.position.y < -5)
+            {
+                rigidbody2D.velocity = new Vector2(0, 0);
+            }
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
